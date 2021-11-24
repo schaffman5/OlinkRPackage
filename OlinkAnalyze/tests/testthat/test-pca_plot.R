abccde5b965bf7e629014ef6732663c4f1655e79
Pascal Pucholt <pascal.pucholt@olink.com>
2021-11-24 16:19:00 +0100

    Use png snapshots for PCA plot unit tests

--- OlinkAnalyze/tests/testthat/test-pca_plot.R
+++ OlinkAnalyze/tests/testthat/test-pca_plot.R
@@ -20,6 +20,39 @@ pca_plot_treatCol_topLoadings <- npx_data1 %>%
                      head(5) %>%
                      pull(OlinkID)})
 
+
+# To use expect_snapshot_file() you'll typically need to start by writing
+# a helper function that creates a file from your code, returning a path
+save_png <- function(code, width = 400, height = 400) {
+  path <- tempfile(fileext = ".png")
+  png(path, width = width, height = height)
+  on.exit(dev.off())
+  code
+
+  path
+}
+
+# You'd then also provide a helper that skips tests where you can't
+# be sure of producing exactly the same output
+expect_snapshot_plot <- function(name, code) {
+  # Other packages might affect results
+  skip_if_not_installed("ggplot2", "2.0.0")
+  # You'll need to carefully think about and experiment with these skips
+
+  name <- paste0(name, ".png")
+
+  # Announce the file before touching `code`. This way, if `code`
+  # unexpectedly fails or skips, testthat will not auto-delete the
+  # corresponding snapshot file.
+  announce_snapshot_file(name = name)
+
+  path <- save_png(code)
+  expect_snapshot_file(path, name)
+}
+
+
+
+
 test_that("olink_pca_plot works", {
 
   # Two Warnings thrown: for dropped assays and droppes samples
@@ -31,9 +64,8 @@ test_that("olink_pca_plot works", {
     )
   )
 
-  skip_on_ci()
-  vdiffr::expect_doppelganger('PCA plot', pca_plot)
-  vdiffr::expect_doppelganger('PCA plot color by treatment', pca_plot_treatCol)
-  vdiffr::expect_doppelganger('PCA plot with loadings', pca_plot_treatCol_topLoadings)
-  vdiffr::expect_doppelganger('PCA plot drop_assays and drop_samples', pca_plot_drop)
+  expect_snapshot_plot('PCA_plot', print(pca_plot))
+  expect_snapshot_plot('PCA_plot_color_by_treatment', print(pca_plot_treatCol))
+  expect_snapshot_plot('PCA_plot_with_loadings', print(pca_plot_treatCol_topLoadings))
+  expect_snapshot_plot('PCA_plot_drop_assays_and_drop_samples', print(pca_plot_drop))
 })
